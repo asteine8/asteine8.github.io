d9748e8f6c77b5590c826fbef4627c4d280d062b
asteine8@gmail.com <psteine3@gmail.com>
2020-01-01 17:26:05 -0800

    test fix 1

--- projects/pronoun-dressing-room/pil-tool/pronoun_sets.js
+++
@@ -1,3 +0,0 @@
-let common_pronoun_sets = [["they","them","their","theirs","theirself"],["he","him","his","his","himself"],["she","her","her","hers","herself"],["xe","xem","xyr","xyrs","xemself"]];
-
-let common_pronouns = ["they","them","their","theirs","theirself","he","him","his","his","himself","she","her","her","hers","herself","xe","xem","xyr","xyrs","xemself"];
