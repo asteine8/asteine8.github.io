f79494d08343a18d3b6b2bf9494452056b157b0f
mincerafter42 <mincerafter42@gmail.com>
2020-02-28 11:56:34 -0800

    Beginning of sentence checks added.
    If {sub} or {pod} are at the beginning of a sentence, they become {Sub} and {Pod} instead.

--- projects/pronoun-dressing-room/pil-tool/parser.js
+++ projects/pronoun-dressing-room/pil-tool/parser.js
@@ -47,7 +47,11 @@ function generateTaggedText() {
 
         // Check for Subjective PRP
         if (taggedwords[w][0].toLowerCase().match(subregex) && taggedwords[w][1].match(/PRP|NN/i)) {
-            taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{sub}");
+            if (w==0||taggedwords[w-1][1]==".") {
+                taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{Sub}");
+            } else {
+                taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{sub}");
+            }
         }
 
         // Check if personal pronoun
@@ -73,7 +77,12 @@ function generateTaggedText() {
                 else if (taggedwords[w+1][1].match(/NN|RB/i)) {
                     // Check the PRP matches expected pronoun
                     if (taggedwords[w][0].toLowerCase().match(pronounSet[2])) {
-                        taggedwords[w][0] = "{pod}";
+                      if (taggedwords[w][0].toLowerCase().match(pronounSet[2])) {
+                        if (w==0||taggedwords[w-1][1]==".") {
+                            taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{Pod}");
+                        } else {
+                            taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{pod}");
+                        }
                     }
                 }
             }
@@ -82,7 +91,12 @@ function generateTaggedText() {
             if (w < taggedwords.length-2) {
                 if (taggedwords[w+1][1].match(/JJ/i) && taggedwords[w+2][1].match(/NN/i)) {
                     if (taggedwords[w][0].toLowerCase().match(pronounSet[2])) {
-                        taggedwords[w][0] = "{pod}";
+                      if (taggedwords[w][0].toLowerCase().match(pronounSet[2])) {
+                        if (w==0||taggedwords[w-1][1]==".") {
+                            taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{Pod}");
+                        } else {
+                            taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{pod}");
+                        }
                     }
                 }
             }
