0be4f4b9b48d25ec85c1b1937bc64e3171c4c4d8
mincerafter42 <mincerafter42@gmail.com>
2020-02-28 18:05:44 -0800

    Compacted capitalization code using ternary operator

--- projects/pronoun-dressing-room/pil-tool/parser.js
+++ projects/pronoun-dressing-room/pil-tool/parser.js
@@ -47,11 +47,7 @@ function generateTaggedText() {
 
         // Check for Subjective PRP
         if (taggedwords[w][0].toLowerCase().match(subregex) && taggedwords[w][1].match(/PRP|NN/i)) {
-            if (w==0||taggedwords[w-1][1]==".") {
-                taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{Sub}");
-            } else {
-                taggedwords[w][0] = taggedwords[w][0].replace(subregex, "{sub}");
-            }
+            taggedwords[w][0] = taggedwords[w][0].replace(subregex, w==0||taggedwords[w-1][1]=="."?"{Sub}":"{sub}");
         }
 
         // Check if personal pronoun
@@ -77,11 +73,7 @@ function generateTaggedText() {
                 else if (taggedwords[w+1][1].match(/NN|RB/i)) {
                     // Check the PRP matches expected pronoun
                     if (taggedwords[w][0].toLowerCase().match(pronounSet[2])) {
-                        if (w==0||taggedwords[w-1][1]==".") {
-                            taggedwords[w][0] = "{Pod}";
-                        } else {
-                            taggedwords[w][0] = "{pod}";
-                        }
+                        taggedwords[w][0] = w==0||taggedwords[w-1][1]=="."?"{Pod}":"{pod}";
                     }
                 }
             }
@@ -90,11 +82,7 @@ function generateTaggedText() {
             if (w < taggedwords.length-2) {
                 if (taggedwords[w+1][1].match(/JJ/i) && taggedwords[w+2][1].match(/NN/i)) {
                     if (taggedwords[w][0].toLowerCase().match(pronounSet[2])) {
-                        if (w==0||taggedwords[w-1][1]==".") {
-                            taggedwords[w][0] = "{Pod}";
-                        } else {
-                            taggedwords[w][0] = "{pod}";
-                        }
+                        taggedwords[w][0] = w==0||taggedwords[w-1][1]=="."?"{Pod}":"{pod}";
                     }
                 }
             }
